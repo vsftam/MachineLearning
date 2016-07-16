bc3cafa1c54f4d626709d0152cebcf8ee3cd6946
Vincent Tam <vincent.tam@outlook.com>
2016-07-16 10:53:40 -0400

    initial commit

---
+++ Puzzles/src/main/scala-2.11/PowerSet.scala
@@ -0,0 +1,21 @@
+/**
+  * Created by Vincent on 7/16/16.
+  */
+object PowerSet {
+
+  def powerSet[A](s: Set[A]) : Set[Set[A]] =
+    if( s.isEmpty ) {
+      Set( Set() )
+    }
+    else {
+      s.flatMap( e => {
+        val ps = powerSet(s - e)
+        ps ++ ps.map(_ + e)
+      } )
+    }
+
+  def main(arg: Array[String]) {
+    println( powerSet(Set("A", "B", "C")))
+  }
+
+}
