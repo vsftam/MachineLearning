ec3fa0179800defb141e77ebf7966f0ca5b4535a
Vincent Tam <vincent.tam@outlook.com>
2016-11-19 22:31:40 -0500

    add NQueens under Puzzles

---
+++ Puzzles/src/main/scala-2.11/vsftam/NQueens.scala
@@ -0,0 +1,36 @@
+package vsftam
+
+object NQueens {
+ 
+    def solveNQueens(n: Int): List[List[(Int, Int)]] = {
+        def placeQueens(k: Int): List[List[(Int, Int)]] = {
+            if(k == 0)
+                List(List())
+            else {
+                for {
+                    queens <- placeQueens(k - 1)
+                    column <- 1 to n
+                    queen = (k, column) 
+                    if( isSafe(queen, queens))
+                } yield queen :: queens 
+            }
+        }
+
+        def isSafe(queen: (Int, Int), queens: List[(Int, Int)]) = {
+            queens forall (q => !inCheck(queen, q))
+        }
+        placeQueens(n)
+    }
+
+    private def inCheck(q1: (Int, Int), q2: (Int, Int)): Boolean = 
+        q1._1 == q2._1 || q1._2 == q2._2 || (q1._1 - q2._1).abs == (q1._2 - q2._2).abs
+
+    private def inCheckSuperQueen(q1: (Int, Int), q2: (Int, Int)): Boolean = 
+        inCheck(q1, q2) ||
+        ((q1._1 - q2._1).abs == 2 && (q1._2 - q2._2).abs == 1) ||
+        ((q1._1 - q2._1).abs == 1 && (q1._2 - q2._2).abs == 2)
+
+    def main(args: Array[String]) {
+        println(solveNQueens(8))
+    }
+}
