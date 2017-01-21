1a4b0f1a5e195522c2a51237d97b48ad0379a228
Vincent Tam <vincent.tam@outlook.com>
2017-01-21 15:30:17 -0500

    use log4s for logging

--- DataAnalyser/build.sbt
+++ DataAnalyser/build.sbt
@@ -3,3 +3,10 @@ name := "DataAnalyser"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.slf4j" % "slf4j-api" % "1.7.22",
+  "org.slf4j" % "slf4j-simple" % "1.7.22",
+  "org.log4s" %% "log4s" % "1.3.4",
+  "org.rogach" %% "scallop" % "2.0.6"
+)
--- DataAnalyser/src/main/scala/dataanalyser/DataAnalyser.scala
+++ DataAnalyser/src/main/scala/dataanalyser/DataAnalyser.scala
@@ -1,19 +1,20 @@
 package dataanalyser
 
-import dataanalyser.YahooFinanceDataSource._
 import dataanalyser.Stat._
+import dataanalyser.YahooFinanceDataSource._
+import org.log4s._
 
 object DataAnalyser extends App {
 
-  println("running main")
-   
   override def main(args: Array[String]) {
     
     val usageStr = "Usage: DataAnalyser -start mmmmyydd -end mmmmyydd --ticker ticker"
-        
+
+    val logger = getLogger
+
     if (args.length != 6) {
-      println("Not sufficent argments ")
-      println(usageStr)
+      logger.info("Not sufficient arguments ")
+      logger.info(usageStr)
       sys.exit(1)
     }
     
@@ -33,15 +34,15 @@ object DataAnalyser extends App {
           nextOption( map ++ Map('end -> value), tail)
         case "-ticker" :: value :: tail =>
           nextOption( map ++ Map('ticker -> value), tail)
-        case option :: tail => println("Unknown option " + option); println(usageStr); sys.exit(1)
+        case option :: tail => logger.info("Unknown option " + option); logger.info(usageStr); sys.exit(1)
       }
     }
     
     val options = nextOption(Map(), arglist)
-    println(options)
+    logger.info(options.toString)
     
     if (options.get('start).isEmpty || options.get('end).isEmpty || options.get('ticker).isEmpty) {
-      println(usageStr)
+      logger.info(usageStr)
       sys.exit(1)
     }
 
@@ -52,22 +53,23 @@ object DataAnalyser extends App {
     val dataSource : YahooFinanceDataSource = new YahooFinanceDataSource()
     val startDate = options.get('start).map(_.toInt).getOrElse(throw new IllegalArgumentException)
     val endDate = options.get('end).map(_.toInt).getOrElse(throw new IllegalArgumentException)
-    val ticker = options.get('ticker).getOrElse(throw new IllegalArgumentException)
+    val ticker = options.getOrElse('ticker, throw new IllegalArgumentException)
       
     val uri = getURI(ticker, startDate, endDate)
-    
-    var data : List[YahooFinanceDataRow] = dataSource.retrieveData(uri)
+
+    val data: List[YahooFinanceDataRow] = dataSource.retrieveData(uri)
 
     /* todo:
      * abstract the calculation
      */
-    println("total data points: "+ data.size)
+    logger.info("total data points: " + data.size)
     
     val K = 2
     val N = 20
 
-    val bblist : List[(Double, Double)] = getBollingerBands(data map (d => d.adjClose), N, K) 
-    bblist map { bb=> println( f"${bb._1}%1.6f" + " : " + f"${bb._2}%1.6f" ) }
+    val bblist: List[(Double, Double)] = getBollingerBands(data map (d => d.adjClose), N, K)
+    val msg = bblist map { bb => f"${bb._1}%1.6f" + " : " + f"${bb._2}%1.6f" }
+    msg.foreach(m => logger.info(m))
  }
   
 }
