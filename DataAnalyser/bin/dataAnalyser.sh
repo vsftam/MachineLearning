9595bc6972bd8099435de84b9fb0d5131459801e
Vincent Tam <vincent.tam@outlook.com>
2017-01-21 12:31:01 -0500

    add program shell script

---
+++ DataAnalyser/bin/dataAnalyser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 start_date[yyyymmdd] end_date[yyyymmdd] ticker"
+  exit 1;
+fi
+cd /Users/vincenttam/Dev/ScalaProjects/DataAnalyser
+echo "Running with start_date $1, end_date $2 and ticker $3"
+sbt "run-main dataanalyser.DataAnalyser -start $1 -end $2 -ticker $3"
+echo "Done"
