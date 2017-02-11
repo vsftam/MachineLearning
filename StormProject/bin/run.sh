156564734d935fa65210f31ddba02ba4ac6aa4b7
Vincent Tam <vincent.tam@outlook.com>
2017-02-11 12:35:38 -0500

    fix run script

--- StormProject/bin/run.sh
+++ StormProject/bin/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 CWD=`pwd`
-echo $CWD
-scala -classpath "$CWD/../target/scala-2.11/stockanalyser-2.11_1.0.jar" com.vsftam.stockanalyser.DataPersister
+cd $CWD/..
+sbt "run-main com.vsftam.stockanalyser.DataPersister"
