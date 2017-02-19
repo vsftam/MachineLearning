2b1e63f41078955a13595d1a5205a68a05cdae40
Vincent Tam <vincent.tam@outlook.com>
2017-02-18 23:14:16 -0500

    Add FreeMonad

--- Puzzles/src/main/scala-2.11/vsftam/ConsoleIO.scala
+++ Puzzles/src/main/scala-2.11/vsftam/ConsoleIO.scala
@@ -28,14 +28,60 @@ object ConsoleIO {
 
   def main(args: Array[String]): Unit = {
 
-    def conversation = Chain[Unit, String](
-      WriteLine("What is your name?"),
-      _ => Chain[String, String](
-        ReadLine(), name =>
-          Chain[Unit, String](WriteLine(s"My name is $name"),
+    def conversation: ConsoleIO[String] =
+      Chain[Unit, String](
+        WriteLine("What is your name?"),
+        _ => Chain[String, String](
+          ReadLine(),
+          name => Chain[Unit, String](
+            WriteLine(s"My name is $name"),
             _ => Pure[String](name))
+        )
       )
-    )
     interpret(conversation)
+  }
+}
+
+object FreeMonad {
+  sealed trait Sequential[F[_], A] {
+    def map[B](f: A => B): Sequential[F, B] = Map(this, f)
+    def flatMap[B](f: A => Sequential[F, B]): Sequential[F, B] = Chain(this, f)
+  }
+  final case class Pure[F[_], A](value: A) extends Sequential[F, A]
+  final case class Effect[F[_], A](fa: F[A]) extends Sequential[F, A]
+  final case class Map[F[_], A, B](v: Sequential[F, A], f: A => B) extends Sequential[F, B]
+  final case class Chain[F[_], A, B](v: Sequential[F, A], f: A => Sequential[F, B]) extends Sequential[F, B]
+
+  trait ConsoleF[A]
+  final case class ReadLine() extends ConsoleF[String]
+  final case class WriteLine(line: String) extends ConsoleF[Unit]
+
+  type ConsoleIO[A] = Sequential[ConsoleF, A]
+
+  def interpret[A](program: ConsoleIO[A]) : A = program match {
+    case Pure(v) => v
+    case Effect(fv) => fv match {
+      case ReadLine() => readLine()
+      case WriteLine(line) => println(line); ()
+    }
+    case Map(v, f) => f(interpret(v))
+    case Chain(v, f) => interpret(f(interpret(v)))
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    def userNameProgram: ConsoleIO[String] =
+      Chain[ConsoleF, Unit, String](
+        Effect[ConsoleF, Unit](WriteLine("What is your name")),
+        _ => Chain[ConsoleF, String, String](
+          Effect[ConsoleF, String](ReadLine()),
+          name => Chain[ConsoleF, Unit, String](
+            Effect[ConsoleF, Unit](WriteLine(s"Hello $name!")),
+            _ => Pure[ConsoleF, String](name)
+          )
+        )
+      )
+
+    interpret(userNameProgram)
   }
 }
