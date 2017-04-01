7441dc25a550582faed7fb9e038f917a24bd7ae7
Vincent Tam <vincent.tam@outlook.com>
2017-04-01 09:48:13 -0400

    Add StackOverflow

---
+++ Coursera/BigDataAnalysisWithScalaAndSpark/stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -0,0 +1,47 @@
+package stackoverflow
+
+import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import java.io.File
+
+@RunWith(classOf[JUnitRunner])
+class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
+
+
+  lazy val testObject = new StackOverflow {
+    override val langs =
+      List(
+        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+    override def langSpread = 50000
+    override def kmeansKernels = 45
+    override def kmeansEta: Double = 20.0D
+    override def kmeansMaxIterations = 120
+  }
+
+  test("testObject can be instantiated") {
+    val instantiatable = try {
+      testObject
+      true
+    } catch {
+      case _: Throwable => false
+    }
+    assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+  test("getKeyWithMostCount") {
+
+    val iter = Seq[(Int, Int)]( (1, 2), (1, 3), (2, 1), (2, 3), (2, 4), (3, 5))
+    assert(StackOverflow.getKeyWithMostCount(iter) == 2)
+  }
+
+  test("getMedian") {
+    val s = Seq[Int](1, 2, 3, 4, 5)
+    assert(StackOverflow.getMedian(s) == 3)
+  }
+}
