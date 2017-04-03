218fff4a25f5c06e9942f4a320e38dde9ddf248e
Vincent Tam <vincent.tam@outlook.com>
2017-04-02 21:07:34 -0400

    Add EstimatePi

--- Puzzles/src/main/scala-2.11/vsftam/EstaimtePi.scala
+++ Puzzles/src/main/scala-2.11/vsftam/EstaimtePi.scala
@@ -1,6 +1,28 @@
+package vsftam
+
+import scala.util.Random
+
 /**
   * Created by vincenttam on 4/2/17.
   */
-object EstaimtePi {
+object EstimatePi {
 
+  def estimatePi(tries: Int) : Double = {
+      var in = 0
+      for(i <- 1 to tries;
+        x = Random.nextDouble();
+        y = Random.nextDouble();
+        if x * x + y * y <= 1
+      ) {
+        in = in + 1
+      }
+      in.toDouble * 4 / tries
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    val pi = estimatePi(500000)
+    System.out.println(s"estimatePi(500000) returns $pi")
+  }
 }
